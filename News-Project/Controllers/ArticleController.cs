5e975d7993b1413efa77da0191baa2eb8bcad7af
Vlad <vlad.popa@swazm.com>
2020-01-06 18:41:41 +0200

    Add project files.

---
+++ News-Project/Controllers/ArticleController.cs
@@ -0,0 +1,199 @@
+﻿using News_Project.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace News_Project.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Article
+        public ActionResult Index()
+        {
+            var articles = db.Articles.Include("Category").Include("User");
+
+            if (TempData.ContainsKey("message"))
+            {
+                ViewBag.message = TempData["message"].ToString();
+            }
+
+            ViewBag.Articles = articles;
+
+            return View();
+        }
+
+        public ActionResult Show(int id)
+        {
+            Article article = db.Articles.Find(id);
+
+            ViewBag.afisareButoane = false;
+            if (User.IsInRole("Editor") || User.IsInRole("Administrator"))
+            {
+                ViewBag.afisareButoane = true;
+            }
+
+            ViewBag.esteAdmin = User.IsInRole("Administrator");
+            ViewBag.utilizatorCurent = User.Identity.GetUserId();
+
+
+            return View(article);
+
+        }
+
+        [Authorize(Roles = "Editor,Administrator")]
+        public ActionResult New()
+        {
+            Article article = new Article();
+
+            // preluam lista de categorii din metoda GetAllCategories()
+            article.Categories = GetAllCategories();
+
+            // Preluam ID-ul utilizatorului curent
+            article.UserId = User.Identity.GetUserId();
+
+
+            return View(article);   
+
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Editor,Administrator")]
+        public ActionResult New(Article article)
+        {
+            article.Categories = GetAllCategories();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Articles.Add(article);
+                    db.SaveChanges();
+                    TempData["message"] = "Articolul a fost adaugat!";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(article);
+                }
+            }
+            catch (Exception e)
+            {
+                return View(article);
+            }
+        }
+
+        [Authorize(Roles = "Editor,Administrator")]
+        public ActionResult Edit(int id)
+        {
+            Article article = db.Articles.Find(id);
+            ViewBag.Article = article;
+            article.Categories = GetAllCategories();
+
+            if (article.UserId == User.Identity.GetUserId() || 
+                User.IsInRole("Administrator"))
+            {
+                return View(article);
+            }
+            else
+            {
+                TempData["message"] = "Nu aveti dreptul sa faceti modificari asupra unui articol care nu va apartine!";
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "Editor,Administrator")]
+        public ActionResult Edit(int id, Article requestArticle)
+        {
+            requestArticle.Categories = GetAllCategories();
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Article article = db.Articles.Find(id);
+                    if (article.UserId == User.Identity.GetUserId() ||
+                        User.IsInRole("Administrator"))
+                    {
+                        if (TryUpdateModel(article))
+                        {
+                            article.Title = requestArticle.Title;
+                            article.Content = requestArticle.Content;
+                            article.Date = requestArticle.Date;
+                            article.CategoryId = requestArticle.CategoryId;
+                            db.SaveChanges();
+                            TempData["message"] = "Articolul a fost modificat!";
+                        }
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        TempData["message"] = "Nu aveti dreptul sa faceti modificari asupra unui articol care nu va apartine!";
+                        return RedirectToAction("Index");
+                    }
+
+                }
+                else
+                {
+                    return View(requestArticle);
+                }
+
+            }
+            catch (Exception e)
+            {
+                return View(requestArticle);
+            }
+        }
+
+        [HttpDelete]
+        [Authorize(Roles = "Editor,Administrator")]
+        public ActionResult Delete(int id)
+        {
+            Article article = db.Articles.Find(id);
+            if (article.UserId == User.Identity.GetUserId() ||
+                User.IsInRole("Administrator"))
+            {
+                db.Articles.Remove(article);
+                db.SaveChanges();
+                TempData["message"] = "Articolul a fost sters!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["message"] = "Nu aveti dreptul sa stergeti un articol care nu va apartine!";
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        [NonAction]
+        public IEnumerable<SelectListItem> GetAllCategories()
+        {
+            // generam o lista goala
+            var selectList = new List<SelectListItem>();
+
+            // Extragem toate categoriile din baza de date
+            var categories = from cat in db.Categories
+                             select cat;
+
+            // iteram prin categorii
+            foreach (var category in categories)
+            {
+                // Adaugam in lista elementele necesare pentru dropdown
+                selectList.Add(new SelectListItem
+                {
+                    Value = category.CategoryId.ToString(),
+                    Text = category.CategoryName.ToString()
+                });
+            }
+
+            // returnam lista de categorii
+            return selectList;
+        }
+    }
+}
---
+++ News-Project/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace News_Project.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Numele categoriei este obligatoriu")]
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Article> Article { get; set; }
+    }
+}
